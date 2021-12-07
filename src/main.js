4e365abf4c2227a3b0d01df0826ca40f722bf07e
Ricky Martinez <RMartinezW16@gmail.com>
2021-12-07 14:39:15 -0500

    working getfortune function

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 
-function getFortune(question) {}
+function getFortune(question) {
+    let answerArray = [];
+    let tellPromise = tell(question);
+    return tellPromise
+    .then((fortune) => {
+        answerArray.push("Your question was: " + question);
+        answerArray.push("Your fortune is: " + fortune);
+
+        return answerArray;
+    })
+    .catch((error) => {
+        return "There was an error: " + error; 
+    });
+}
 
 function fullSession(question) {}
 
